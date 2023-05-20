5c9791ea4d7761d16c98544139f26c051fa8eb7e
Supreeti <sksupreetk11@gmail.com>
2023-05-20 15:40:43 +0530

    add style

--- src/modules/game.js
+++ src/modules/game.js
@@ -3,9 +3,9 @@ const game = async (gameUrl, scores) => {
   await response.json().then((newData) => {
     scores.replaceChildren();
     newData.result.forEach((score) => {
-      const li = document.createElement('li');
+      const li = document.createElement('tr');
       li.classList.add('score');
-      li.innerHTML = `${score.user}: ${score.score}`;
+      li.innerHTML = `<tr><td>${score.user}</td> <td>${score.score}</td></tr>`;
       scores.appendChild(li);
     });
   });
